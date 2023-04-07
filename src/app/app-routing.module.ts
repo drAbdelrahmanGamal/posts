9009bbe96d049981eb154cdf1e1af62494711904
drAbdelrahmanGamal <dr.abdo2022@outlook.com>
2023-04-07 14:32:10 +0200

    creating routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostsComponent } from './components/posts/posts.component';
+import { HistoryComponent } from './components/history/history.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PostsComponent },
+  { path: 'history', component: HistoryComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
