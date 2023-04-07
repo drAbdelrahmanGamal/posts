23914654438dd7180dd6a2648374d865942d26b7
drAbdelrahmanGamal <dr.abdo2022@outlook.com>
2023-04-07 14:57:43 +0200

    adding posts

--- src/app/components/post-item/post-item.component.ts
+++ src/app/components/post-item/post-item.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Post } from 'src/app/models/post';
 
 @Component({
   selector: 'app-post-item',
   templateUrl: './post-item.component.html',
-  styleUrls: ['./post-item.component.css']
+  styleUrls: ['./post-item.component.css'],
 })
 export class PostItemComponent {
+  @Input() post: Post;
 
+  constructor() {
+    this.post = {
+      id: 0,
+      title: '',
+      body: '',
+      votes: 1,
+    };
+  }
 }
--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/models/post';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.css']
+  styleUrls: ['./posts.component.css'],
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
+  title: string = 'Posts';
+  posts: Post[] = [];
 
+  ngOnInit(): void {
+    this.posts = [
+      {
+        id: 1,
+        title: 'My first post',
+        body: 'Just testing this out!',
+        votes: 1,
+      },
+      {
+        id: 2,
+        title: "What's your favorite front-end framework?",
+        body: 'My favorite is Angular. What do you enjoy working with?',
+        votes: 1,
+      },
+      {
+        id: 3,
+        title: 'ngOnInit is great',
+        body: 'This lifecycle method gets called automatically upon component initialization!',
+        votes: 1,
+      },
+    ];
+  }
 }
